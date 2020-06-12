3e70b13f1072523234c4a53b2a61ccf397d0497a
John Morris <jmorris@sterlingdb.com>
2020-06-12 18:50:24 -0500

    internal improvements

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # fmp-node
 FileMaker Data API class for node.js
+
+
+create a .env file with these paramaters:
+
+    FMP_USERNAME='john.morris'
+    FMP_PASSWORD='myP@$$w0rd'
+    FMP_HOST='https://domainname.com'
+    FMP_DATABASE='MyFileMakerDatabaseName'
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 const axios = require('axios');
 
-class FmpApi{
+class FmpDataApi{
 
     constructor(){
         this.headers = {
@@ -11,90 +11,106 @@ class FmpApi{
         };
         this.token = null;
         this.layout = null;
-        this.baseUrl = `${process.env.FMP_BASE_URL}/databases/${process.env.FMP_DATABASE}`;
+        this.database = process.env.FMP_DATABASE;
+        this.baseUrl = `${process.env.FMP_HOST}/fmi/data/v1/databases/${this.database}`;
+        this.httpResponce = {};
+        this.error = null;
+        this.message = null;
+        this.response = null;
+        this.status = null;
+        this.statusText = null;
     }
 
-    setLayout(layout){
-        this.layout = layout;
+    setLayout(name){
+        this.layout = name;
     }
-
+    setDatabase(name){
+      this.database = name;
+  }
     async login() {
         const authBuffer = new Buffer.from(`${process.env.FMP_USERNAME}:${process.env.FMP_PASSWORD}`);
         const base64Auth = authBuffer.toString('base64');
     
         const axiosUrl = `${this.baseUrl}/sessions`;
-        let httpResponce = {};
+        // let httpResponce = {};
       
         try {
-          httpResponce = await axios
+          this.httpResponce = await axios
             .post(axiosUrl,null,{headers: {'Content-Type': 'application/json', 'Authorization': `Basic ${base64Auth}`}})
             .then(response => {
                 this.headers.Authorization = `Bearer ${response.data.response.token}`
                 this.token = response.data.response.token;
+
+                this.message = response.data.messages;
+                this.response = response.data.response;
+  
               return response.data.response.token;
             });
         } catch (error) {
-          httpResponce = error;
+          this.httpResponce = error;
         } finally {
-          return httpResponce;
+          return this.httpResponce;
         }
     }
     async logout(fmpToken) {
     
         const axiosUrl = `${this.baseUrl}/sessions/${this.token}`;
-        let httpResponce = {};
       
         try {
-          httpResponce = await axios
+          this.httpResponce = await axios
             .delete(axiosUrl)
             .then(response => {
+                this.message = response.data.messages;
+                this.response = response.data.response;
                 return response.data;
             });
         } catch (error) {
-          httpResponce = error;
+          this.httpResponce = error;
         } finally {
-          return httpResponce;
+          return this.httpResponce;
         }
     }
     async getRecord(fmpId) {
         const axiosUrl = `${this.baseUrl}/layouts/${this.layout}/records/${fmpId}`;
-        let httpResponce = {};
     
         try {
-          httpResponce = await axios
+          this.httpResponce = await axios
             .get(
                 axiosUrl,
                 {headers:this.headers},
             )
             .then(response => {
+              this.message = response.data.messages;
+              this.response = response.data.response;
               return response.data;
             });
         } catch (error) {
-          httpResponce = error;
+          this.httpResponce = error;
         } finally {
-          return httpResponce;
+          return this.httpResponce;
         }
     }
     async addRecord(recordData) {
         const axiosUrl = `${this.baseUrl}/layouts/${this.layout}/records`;
-        let httpResponce = {};
     
         try {
-          httpResponce = await axios
+          this.httpResponce = await axios
             .post(
                 axiosUrl,
                 recordData,
                 {headers:this.headers},
             )
             .then(response => {
+              this.message = response.data.messages;
+              this.response = response.data.response;
               return response.data;
             });
         } catch (error) {
-          httpResponce = error;
+          this.httpResponce = error;
         } finally {
-          return httpResponce;
+          return this.httpResponce;
         }
     }
 }
 
- export { FmpApi };
+ export { FmpDataApi };
